986015fe3ade109f6e7a27c39057e14398301141
cxx <sinksmell@163.com>
2025-10-28 12:01:39 +0000

    feat: update go mod

--- parser.go
+++ parser.go
@@ -16,8 +16,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 
-	"github.com/httprunner/httprunner/v4/hrp/internal/builtin"
-	"github.com/httprunner/httprunner/v4/hrp/internal/code"
+	"github.com/sinksmell/httprunner/v4/hrp/internal/builtin"
+	"github.com/sinksmell/httprunner/v4/hrp/internal/code"
 )
 
 func newParser() *Parser {
